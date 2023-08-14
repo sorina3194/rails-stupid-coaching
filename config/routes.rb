b5554442b96da45fa392272526257ebd52e88430
sorina3194 <sorinatudor94@gmail.com>
2023-08-14 15:10:54 +0200

    first rails exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: "questions#ask"
+  # get "/path", to: "controller#actions" triggers view with corresponding name (ask or answer)
+  get "/ask", to: "questions#ask"
+  get "/answer", to: "questions#answer"
   # Defines the root path route ("/")
   # root "articles#index"
 end
